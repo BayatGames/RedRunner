3f5b13a01e716d4add78c69c892c0b58e2c7bdde
salberico <sam.alberico@gmail.com>
2019-11-09 16:29:39 -0500

    added ui click regions

---
+++ Assets/Scripts/RedRunner/Characters/CharacterInput.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterInput : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+
+        }
+    }
+}
---
+++ Assets/Scripts/RedRunner/UI/RegionClick.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+[RequireComponent(typeof(BoxCollider2D))]
+public class RegionClick : MonoBehaviour
+{
+    [SerializeField]
+    private UnityEvent onClick = new UnityEvent();
+
+    void Start()
+    {
+        BoxCollider2D bc = GetComponent<BoxCollider2D>();
+        RectTransform rect = GetComponent<RectTransform>();
+        bc.size = new Vector2(rect.rect.width, rect.rect.height);
+    }
+
+    void OnMouseDown()
+    {
+        onClick.Invoke();
+    }
+
+}
