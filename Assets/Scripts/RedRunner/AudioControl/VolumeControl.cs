4e067930c9b5edd6a23933e6639d02e10fe98aca
CaseyTea <cthatsanaphonh@csu.fullerton.edu>
2021-03-19 15:33:05 -0700

    Feature add for volume slider
    Added script for volume slider

---
+++ Assets/Scripts/RedRunner/AudioControl/VolumeControl.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio; 
+
+public class VolumeControl : MonoBehaviour
+{
+    [SerializeField]
+    private AudioMixer mixer;
+    [SerializeField]
+    private string audioTargetName = "";
+  
+    public void SetVolumeLevel(float sliderValue)
+    {
+        mixer.SetFloat(audioTargetName, Mathf.Log10(sliderValue) * 20);
+    }
+    
+}
